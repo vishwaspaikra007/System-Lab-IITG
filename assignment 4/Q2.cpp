32f01e3a2b2ff7fc1e686e67ffe699206c8d5eaf
Evanstar <cheldridwar@gmail.com>
2023-04-06 12:40:56 +0530

    debuged question 2

--- assignment 4/Q2.cpp
+++ assignment 4/Q2.cpp
@@ -5,7 +5,7 @@
 #include <sys/msg.h>
 #include<unistd.h>
 
-#define MSGSZ 256
+#define MSGSZ 50
 
 using namespace std;
 
@@ -26,11 +26,11 @@ typedef struct quesBuff{
     char qText[MSGSZ];
 } quesBuff;
 
-void grade(int nQues, char ansKey[], ansBuff ans[]) //function to calculate and display the grade
+void grade(int nQues, string ansKey, ansBuff ans[]) //function to calculate and display the grade
 {
 	int total = 0;
 	printf("******Grading******\n100 to 90 - A\n89 to 80 - B\n79 to 70 - C\n69 to 30 - D\nLess than 30 - FF\n*******************\n");
-	printf("ans key :%s\n", ansKey);
+	printf("ans key :%s\n", ansKey.c_str());
 	for (int i = 0; i < nQues; i++) //evaluating the answers
 	{
 		if(ans[i].ansResponse[0] == ansKey[i]) total++;
@@ -38,22 +38,22 @@ void grade(int nQues, char ansKey[], ansBuff ans[]) //function to calculate and 
 	cout << "total : " << total << endl;
 	float percentage = ((float)total/nQues)*100;
 	cout << "percentage: "<<percentage << endl;
-	if(percentage >= 90)		
+	if(percentage >= 50)		
 	{
 		printf("grade : A\n");
 		gradeDis[ans[0].pid] = "A";
 	}
-	else if(percentage >= 80) 	
+	else if(percentage >= 40) 	
 	{
 		printf("grade : B\n");
 		gradeDis[ans[0].pid] = "B";
 	}
-	else if(percentage >= 70)	
+	else if(percentage >= 30)	
 	{
 		printf("grade : C\n");
 		gradeDis[ans[0].pid] = "C";
 	}
-	else if(percentage >= 30)	
+	else if(percentage >= 25)	
 	{
 		printf("grade : D\n");
 		gradeDis[ans[0].pid] = "D";
@@ -68,9 +68,11 @@ void grade(int nQues, char ansKey[], ansBuff ans[]) //function to calculate and 
 
 int main()
 {
+	srand(0);
+	unsigned int seed = time(0); 
 	int msqid1, msqid2;
-    key_t key = 131; //129 		//unique token for every message queue
-	key_t key2 = 130; //128		
+    key_t key = 133; //129 		//unique token for every message queue
+	key_t key2 = 132; //128		
 	int noOfStudents, noOfQues;
 	message_buf sbuf, sbuf2, rbuf, rbuf2;	//send and read buffers
 	
@@ -79,8 +81,13 @@ int main()
 	cout << "\nenter the number of questions: ";
 	cin >> noOfQues;
 	cout << "\nenter the answer keys: ";
-	char ansKey[noOfQues];
-	scanf("%s", ansKey);
+	string ansKey = "";
+	// scanf("%s", ansKey);
+	for(int i = 0; i < noOfQues; i++) //random answer key 
+	{
+		ansKey += ((rand() % 4  + 1) + 48);
+	}
+	printf("anskey: %s\n", ansKey.c_str());
 	quesBuff qSBuf[noOfQues], qRBuf[noOfQues]; //send and read buffers
 	ansBuff aSBuff[noOfQues], aRBuff[noOfQues]; //send and read buffers
 	char q[MSGSZ]; //index for each question
@@ -98,7 +105,7 @@ int main()
     	exit(1);
     }
 
-	if(msgsnd(msqid1, &qSBuf, MSGSZ*noOfQues, IPC_NOWAIT) < 0) //sending the questions to child process
+	if(msgsnd(msqid1, &qSBuf, 2*MSGSZ*noOfQues, IPC_NOWAIT) < 0) //sending the questions to child process
 	{
 		perror("msgsnd123");
 		exit(1);
@@ -119,28 +126,30 @@ int main()
 		}	
 		if(cPid == 0)
 		{
-			srand(time(NULL));
+			srand(getpid());
 			cout <<"\n";
-			if(msgrcv(msqid1, &qRBuf, MSGSZ*noOfQues, 0, 0) < 0) 
+			if(msgrcv(msqid1, &qRBuf, 2*MSGSZ*noOfQues, 0, 0) < 0) 
 				{
 					perror("msgrcv");
 					exit(1);
 				}
-			for(int i = 0; i < noOfQues; i++)
-			{	
-				printf("question %d received from parent :%s\n", i+1, qRBuf[i].qText);
-			}
+			// for(int i = 0; i < noOfQues; i++)
+			// {	
+				// printf("question %d received from parent :%s\n", i+1, qRBuf[i].qText);
+			// }
+			printf("questions received from parent\n");
+
 			msgctl(msqid1, IPC_RMID, NULL); //empty the message queue after receiving questions from parent
 			int temp1; //random choice from 1 to 4
 			for (int i = 0; i < noOfQues; i++) //creating the random answers
 			{
 				aSBuff[i].pid = getpid();
-				temp1 = (rand() % (4 - 1 + 1)) + 1;
+				temp1 = ((rand()) % 4 ) + 1;
 				aSBuff[i].ansResponse[0] = temp1 + 48;
 				aSBuff[i].ansResponse[1] = '\0';
 			}
 
-			if(msgsnd(msqid2, &aSBuff, noOfQues*MSGSZ, IPC_NOWAIT) < 0) // sending the random answers to parent
+			if(msgsnd(msqid2, &aSBuff, 2*noOfQues*MSGSZ, IPC_NOWAIT) < 0) // sending the random answers to parent
 			{
 				perror("msgsndfromchild");
 				exit(1);
@@ -150,8 +159,8 @@ int main()
 		}
 		else 
 		{
-			sleep(1);
-			if(msgrcv(msqid2, &aRBuff, noOfQues*MSGSZ, 0, 0) < 0) //parent receiving the ans in aRBuff
+			//sleep(1);
+			if(msgrcv(msqid2, &aRBuff, 2*noOfQues*MSGSZ, 0, 0) < 0) //parent receiving the ans in aRBuff
 			{
 				perror("msgrcv111");
 				exit(1);
@@ -171,7 +180,7 @@ int main()
         		perror("msgget");
         		exit(1);
     		}
-			if(msgsnd(msqid1, &qSBuf, MSGSZ*noOfQues, IPC_NOWAIT) < 0) 
+			if(msgsnd(msqid1, &qSBuf, 2*MSGSZ*noOfQues, IPC_NOWAIT) < 0) 
 			{
 				perror("msgsndfromparent");
 				exit(1);
