4e9c75f550e3a06edeacdfbe9a8955d3570834b9
Abhik Khan <abhik.khan.1997@gmail.com>
2023-04-20 21:10:57 +0530

    Q2 is added.

---
+++ assignment 5/Q2.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <mutex>
+#include <semaphore.h>
+
+sem_t bridge;
+sem_t dir;
+
+using namespace std;
+
+#define MAX_THREADS 20
+
+void northBoundPeople(int id){
+    this_thread::sleep_for(chrono::milliseconds(rand() % 100)); // waiting before the crossing bridge
+    sem_wait(&bridge); // try to aquire semaphore
+    this_thread::sleep_for(chrono::milliseconds(rand() % 100)); // crossing bridge
+
+    cout<<"North bound "<<id<<" is trying to cross the bridge.\n";
+
+    sem_wait(&dir); // try to aquire semaphore
+    this_thread::sleep_for(chrono::milliseconds(rand() % 100)); // crossed the bridge
+
+    cout<<"North bound "<<id<<" crossed the bridge.\n";
+
+    sem_post(&dir);
+    sem_post(&bridge);
+}
+
+void southBoundPeople(int id){
+    this_thread::sleep_for(chrono::milliseconds(rand() % 100)); // waiting before crossing the bridge
+    sem_wait(&dir); // try to aquire semaphore
+    this_thread::sleep_for(chrono::milliseconds(rand() % 100)); // crossing the bridge
+
+    cout<<"South bound "<<id<<" is trying to cross the bridge.\n";
+
+    sem_wait(&bridge); // try to aquire semaphore
+    this_thread::sleep_for(chrono::milliseconds(rand() % 100)); // crossed the bridge
+
+    cout<<"South bound "<<id<<" crossed the bridge.\n";
+
+    sem_post(&bridge);
+    sem_post(&dir);
+}
+
+
+int main(int argc, char **argv){
+    thread persons[MAX_THREADS];
+    sem_init(&bridge, 0, 1);
+    int Case = stoi(argv[1]);
+
+    if(Case == 2)sem_init(&dir, 0, 1);
+    else sem_init(&dir, 0, MAX_THREADS);
+
+    for(int i = 0; i< MAX_THREADS; ++i){
+        if(i%2 == 0)persons[i] = thread(northBoundPeople, i+1);
+        else persons[i] = thread(southBoundPeople, i+1);
+    }
+
+    for(int i = 0; i< MAX_THREADS; ++i){
+        persons[i].join();
+    }
+
+    return 0;
+}
